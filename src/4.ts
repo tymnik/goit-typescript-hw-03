ddcc92de96c97cc48e644bbecde33a4c7c2c4a53
Veronika Timakova <tymnik@ukr.net>
2024-01-04 23:01:29 +0300

    tasks done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,75 @@
-const key = new Key();
+abstract class House {
+  door: boolean = false;
+  key: Key;
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  constructor(key: Key) {
+    this.key = key;
+  }
 
-house.openDoor(person.getKey());
+  abstract openDoor(enteredKey: Key): void;
 
-house.comeIn(person);
+  comeIn(person: Person): void {
+    if (this.door) {
+      console.log(`${person.getKey().getSignature()} entered the house.`);
+    } else {
+      console.log("The door is closed.");
+    }
+  }
+}
 
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(enteredKey: Key): void {
+    if (enteredKey.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is now open.");
+    } else {
+      console.log("Invalid key.");
+    }
+  }
+}
+
+class Key {
+  private signature: string;
+
+  constructor() {
+    this.signature = this.generateRandomSignature();
+  }
+
+  private generateRandomSignature(): string {
+    return Math.random().toString(36).substring(2);
+  }
+
+  getSignature(): string {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+const keyForMyHouse = new Key();
+const myHouse = new MyHouse(keyForMyHouse);
+
+myHouse.openDoor(keyForMyHouse);
+const personWithValidKey = new Person(keyForMyHouse);
+myHouse.comeIn(personWithValidKey);
+
+const anotherKey = new Key();
+myHouse.openDoor(anotherKey);
+const personWithInvalidKey = new Person(anotherKey);
+myHouse.comeIn(personWithInvalidKey);
 
 export {};
